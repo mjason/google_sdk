de8479413ab72a4186ddb16104c666dbe9c4beea
mj <tywf91@gmail.com>
2013-10-24 13:24:02 +0800

    update 0.0.4

--- lib/glass_sdk/oauth.rb
+++ lib/glass_sdk/oauth.rb
@@ -6,42 +6,43 @@ module GlassSdk
 
   # 用户认证基类
   class OAuth
-    def initialize
-      @base_uri = "https://accounts.google.com"
-      @redirect_uri = GlassSdk.redirect_uri
-      @access_type = 'offline'
-      @approval_prompt = 'force'
 
-      @client_id = GlassSdk.client_id
-      @client_secret = GlassSdk.client_secret
-      @scope = GlassSdk.scope
-    end
-    
-    # 获取用户登录的url
-    def login_url
-      params = []
-      {
-        response_type: 'code',
-        client_id: @client_id,
-        redirect_uri: @redirect_uri,
-        scope: @scope.join("+"),
-        access_type: @access_type,
-        approval_prompt: @approval_prompt
-      }.each do |key, value|
-        params << "#{key}=#{value}"
+    class << self
+      @@base_uri = "https://accounts.google.com"
+      @@redirect_uri = GlassSdk.redirect_uri
+      @@access_type = 'offline'
+      @@approval_prompt = 'force'
+
+      @@client_id = GlassSdk.client_id
+      @@client_secret = GlassSdk.client_secret
+      @@scope = GlassSdk.scope
+      
+      # 获取用户登录的url
+      def login_url
+        params = []
+        {
+          response_type: 'code',
+          client_id: @@client_id,
+          redirect_uri: @@redirect_uri,
+          scope: @@scope.join("+"),
+          access_type: @@access_type,
+          approval_prompt: @@approval_prompt
+        }.each do |key, value|
+          params << "#{key}=#{value}"
+        end
+        return "#{@@base_uri}/o/oauth2/auth?#{params.join("&")}"      
       end
-      return "#{@base_uri}/o/oauth2/auth?#{params.join("&")}"      
-    end
 
-    # 根据code 换取token
-    def user_token(code)
-      RestClient.post "#{@base_uri}/o/oauth2/token", {
-        code: code,
-        client_id: @client_id,
-        client_secret: @client_secret,
-        redirect_uri: @redirect_uri,
-        grant_type: 'authorization_code'
-      }
+      # 根据code 换取token
+      def user_token(code)
+        RestClient.post "#{@@base_uri}/o/oauth2/token", {
+          code: code,
+          client_id: @@client_id,
+          client_secret: @@client_secret,
+          redirect_uri: @@redirect_uri,
+          grant_type: 'authorization_code'
+        }
+      end
     end
 
   end
---
+++ test/dummy/config/initializers/glass_sdk.rb
@@ -0,0 +1,15 @@
+GlassSdk.setup do |config|
+  
+  config.client_id = "your client_id"
+  config.client_secret = "your client_secret"
+
+  # your need google api scope
+  config.scope = %w{
+    https://www.googleapis.com/auth/glass.timeline
+    https://www.googleapis.com/auth/userinfo.profile
+  }
+
+  # your need html templaet path
+  config.template_path = "#{Rails.root}/lib/views"
+  config.redirect_uri = "https://localhost:3000/oauth2callback" #setting your oauth callback
+end
